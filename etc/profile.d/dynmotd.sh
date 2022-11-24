1ab1d7f01b57bf7ddb89362d4eb814b815d246ae
mctaylors <volkovvladislav8@gmail.com>
2022-11-24 19:10:41 +0300

    Added sessions function & removed sessions.sh 	modified:   etc/profile.d/dynmotd.sh 	deleted:    usr/local/lib/dynmotd/sessions.sh

--- etc/profile.d/dynmotd.sh
+++ etc/profile.d/dynmotd.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
-lastlog () {
+sessions() {
+  userlist="$(users | sed 's/\s\+/\n/g')"
+  uniqusers="$(echo "$userlist" | uniq)"
+  uniqcount="$(echo "$uniqusers" | wc -l)"
+  for i in $(seq 1 $uniqcount); do
+    uniquser="$(echo "$uniqusers" | sed "${i}q;d")"
+    sessions="$(echo "$userlist" | grep "$uniquser" | wc -l)"
+    echo -n "$uniquser"
+    [[ 1 < $sessions ]] && echo -n " (${sessions})"
+    [[ $i < $uniqcount ]] && echo -n ", "
+  done
+  echo
+}
+
+lastlog() {
   if [[ -f ~/.lastlog.dynmotd ]]; then
     date -d "$(cat ~/.lastlog.dynmotd)" '+%A, %d %B %Y %R %Z'
   else
     echo "never"
   fi
-  date > ~/.lastlog.dynmotd
+  date >~/.lastlog.dynmotd
 }
 
 dependenciesDir=../../usr/local/lib/dynmotd
@@ -14,13 +28,13 @@ dependenciesDir=../../usr/local/lib/dynmotd
 cat ${dependenciesDir}/header.txt
 
 echo
-echo -e "    MOTD:\t\t" `cat ${dependenciesDir}/motd.txt`
+echo -e "    MOTD:\t\t" $(cat ${dependenciesDir}/motd.txt)
 echo
-echo -e "    Date and time:\t" `echo $(date '+%A, %d %B %Y %R %Z')`
-echo -e "    Uptime:\t\t" `uptime -p`
-echo -e "    Kernel info:\t" `uname -smr`
-echo -e "    Disk space used:\t" `df -h | grep sda3 | awk '{print $5}'`"   "`df -h | grep sda3 | awk '{print $3}'` / `df -h | grep sda3 | awk '{print $2}'` " ("`df -h | grep sda3 | awk '{print $4}'` "своб.)"
+echo -e "    Date and time:\t" $(echo $(date '+%A, %d %B %Y %R %Z'))
+echo -e "    Uptime:\t\t" $(uptime -p)
+echo -e "    Kernel info:\t" $(uname -smr)
+echo -e "    Disk space used:\t" $(df -h | grep sda3 | awk '{print $5}')"   "$(df -h | grep sda3 | awk '{print $3}') / $(df -h | grep sda3 | awk '{print $2}') " ("$(df -h | grep sda3 | awk '{print $4}') "своб.)"
 echo
-echo -e "    Hostname:\t\t" `hostname`
-echo -e "    Sessions:\t\t" `${dependenciesDir}/sessions.sh`
+echo -e "    Hostname:\t\t" $(hostname)
+echo -e "    Sessions:\t\t $(sessions)"
 echo -e "    Last login:\t\t $(lastlog)"
